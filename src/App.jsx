10bbe51b26da523b9784f5ef9ceb93929e71db7a
neeti-nk47 <neeti.nk47@gmail.com>
2023-08-24 22:07:36 +0530

    footer in mobile only

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ function App() {
         <Banking />
         <Boost />
         <Investing />
-        <MobileLastSection />
       </div>
     </>
   );
--- src/pages/MobileLastSection.jsx
+++ src/pages/MobileLastSection.jsx
@@ -15,6 +15,56 @@ const MobileLastSection = () => {
             Savings Bank, Members FDIC.
           </p>
         </div>
+        <div className="flex flex-row justify-center items-center gap-5">
+          <button
+            className={`p-5 py-3 flex items-center justify-center gap-3 border-[1px] border-white rounded-md`}
+          >
+            <img
+              src="/assets/apple-green.png"
+              alt="Apple icon"
+              className="w-4"
+            />
+            <div className={`text-white font-Agrandir text-[9px]`}>
+              APP STORE
+            </div>
+          </button>
+          <button
+            className={`p-5 py-3 flex items-center justify-center gap-3 border-[1px] border-white rounded-md`}
+          >
+            <img
+              src="/assets/playstore-green.png"
+              alt="PLay Store icon"
+              className="w-4"
+            />
+            <div className={`text-white font-Agrandir text-[9px]`}>
+              PLAY STORE
+            </div>
+          </button>
+        </div>
+
+        <div className="flex flex-row items-center gap-10">
+          <button>
+            <img
+              src={`/assets/twitch-white.png`}
+              alt="Twitch icon"
+              className="w-7"
+            />
+          </button>
+          <button>
+            <img
+              src={`/assets/twitter-white.png`}
+              alt="Twitter icon"
+              className="w-7"
+            />
+          </button>
+          <button>
+            <img
+              src={`/assets/instagram-white.png`}
+              alt="Instagram icon"
+              className="w-7"
+            />
+          </button>
+        </div>
       </div>
     </section>
   );
